01a132ed62f11becb0672f5c84848df894609bf2
Maycon Eduardo <mayconeduardo16@gmail.com>
2021-05-02 19:04:09 -0300

    Refatorando os Services

--- src/services/MessagesService.ts
+++ src/services/MessagesService.ts
@@ -1,4 +1,5 @@
-import { getCustomRepository } from "typeorm";
+import { getCustomRepository, Repository } from "typeorm";
+import { Message } from "../entities/Message";
 import { MessagesRepository } from "../repositories/MessagesRepository";
 
 interface IMessageCreate {
@@ -8,25 +9,26 @@ interface IMessageCreate {
 }
 
 class MessagesService {
-    async create({ admin_id, user_id, text }: IMessageCreate) {
-        const messagesRepository = getCustomRepository(MessagesRepository)
+    private messagesRepository: Repository<Message>
 
-        const message = messagesRepository.create({
+    constructor() {
+        this.messagesRepository = getCustomRepository(MessagesRepository)
+    }
+    async create({ admin_id, user_id, text }: IMessageCreate) {
+        const message = this.messagesRepository.create({
             admin_id,
             user_id,
             text
         })
 
-        await messagesRepository.save(message)
+        await this.messagesRepository.save(message)
 
         return message
     }
 
     async listByUser(user_id: string) {
-        const messagesRepository = getCustomRepository(MessagesRepository)
-
-        const list = await messagesRepository.find({
-            where: {user_id},
+        const list = await this.messagesRepository.find({
+            where: { user_id },
             relations: ["user"]
         })
 
